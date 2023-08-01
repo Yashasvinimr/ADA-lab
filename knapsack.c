8e7de8aea43c65da2c354368f5bd3d29c52940df
Yashasvinimr <117368989+Yashasvinimr@users.noreply.github.com>
2023-08-01 14:33:19 +0530

    Add files via upload

--- knapsack.c
+++ knapsack.c
@@ -1,38 +1,64 @@
 #include<stdio.h>
-int max(int a, int b) { return (a > b)? a : b; }
-int knapSack(int W, int wt[], int val[], int n)
+#include<conio.h>
+int n,m,w[10],p[10],v[10][10],x[10];
+void knapsack()
 {
-   int i, w;
-   int K[n+1][W+1];
-   for (i = 0; i <= n; i++)
-   {
-       for (w = 0; w <= W; w++)
-       {
-           if (i==0 || w==0)
-               K[i][w] = 0;
-           else if (wt[i-1] <= w)
-                 K[i][w] = max(val[i-1] + K[i-1][w-wt[i-1]],  K[i-1][w]);
-           else
-                 K[i][w] = K[i-1][w];
-       }
-   }
-   return K[n][W];
-}
-int main()
-{
-    int i, n, val[20], wt[20], W;
-
-    printf("Enter number of items:");
-    scanf("%d", &n);
-
-    printf("Enter value and weight of items:\n");
-    for(i = 0;i < n; ++i){
-     scanf("%d%d", &val[i], &wt[i]);
+    int i,j;
+    for(i=0;i<=n;i++)
+    {
+        for(j=0;j<=m;j++)
+        {
+            if(i==0||j==0)
+                v[i][j]=0;
+            if(w[i]>j)
+                v[i][j]=v[i-1][j];
+            else
+                v[i][j]=(v[i-1][j]>v[i-1][j-w[i]]+p[i])?v[i-1][j]:v[i-1][j-w[i]]+p[i];
+        }
     }
-
-    printf("Enter size of knapsack:");
-    scanf("%d", &W);
-
-    printf("Profit is %d", knapSack(W, wt, val, n));
-    return 0;
+    object_selected();
+}
+void object_selected()
+{
+    int i,j;
+    printf("Optimal solution:%d\n",v[n][m]);
+    for(i=1;i<=n;i++)
+        x[i]=0;
+        i=n;
+        j=m;
+    while(i!=0 && j!=0)
+    {
+        if(v[i][j]!=v[i-1][j])
+        {
+            x[i]=1;
+            j=j-w[i];
+        }
+        i--;
+    }
+    for(i=1;i<=n;i++)
+    {
+        if(x[i]==1)
+        {
+            printf("Object %d is selected\n",i);
+        }
+    }
+}
+void main()
+{
+    int i;
+    printf("Emter number of objects:");
+    scanf("%d",&n);
+    printf("\nMaximum capacity:");
+    scanf("%d",&m);
+    printf("\nEnter weights of the objects:");
+    for(i=1;i<=n;i++)
+    {
+        scanf("%d",&w[i]);
+    }
+    printf("\nEnter profits of the objects:");
+    for(i=1;i<=n;i++)
+    {
+        scanf("%d",&p[i]);
+    }
+    knapsack();
 }
